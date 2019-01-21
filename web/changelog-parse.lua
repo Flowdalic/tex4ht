7b76dd1022219adbfaba0fb0f0c56b94852840f8
michal_h21 <michal_h21@b3dcb3a8-5acb-4090-8ad3-ac17c6a25672>
2019-01-21 10:40:54 +0000

    Create changes.html from the ChangeLog
    git-svn-id: https://svn.gnu.org.ua/sources/tex4ht/trunk@490 b3dcb3a8-5acb-4090-8ad3-ac17c6a25672

---
+++ web/changelog-parse.lua
@@ -0,0 +1,66 @@
+-- convert changelog to HTML
+
+local function find_header(buffer, position)
+  local start, stop, date, author, email = buffer:find("(%d+%-%d+%-%d+)%s+(.-)%<([^%>]+)%>", position)
+  return start, stop + 1, date, author, email
+end
+
+local function parse_entry(buffer, position)
+  local _, pos, date, author, email = find_header(buffer, position)
+  local entry = {date = date, author = author, email = email}
+  -- find text between two headers
+  local next_header = find_header(buffer, pos)
+  entry.text = buffer:sub(pos, next_header - 1)
+  return entry, pos + 1
+end
+
+local escapes = {["<"] = "&lt;", [">"] = "&gt;", ["&"] = "&amp;"}
+
+local function print_entry(entry)
+  -- print just the date and the entry text
+  print("<tr>")
+  print("<td>".. entry.date .. "</td>")
+  local end_par = ""
+  local escaped = entry.text:gsub("([%<%>%&])", function(a)
+    return escapes[a]
+  end)
+  local text = escaped--:gsub("^%s*", "") -- remove space at the beginning
+  :gsub("\n%s*%*%s+", function(star) -- replace "*" characters with paragraphs in the changelog entry
+    local replace = end_par .. "<p>"
+    end_par = "\n</p>\n"
+    return replace
+  end)
+  :gsub("%s*$", "") -- remove space at the end
+  .. "\n</p>" -- we need to close the paragraph explicitly
+  -- escape file names
+  text = text:gsub("<p>(.-):", "<p><em>%1</em>:")
+  -- escape links
+  text = text:gsub("(http[^%s]+)", "<br><a href='%1'>%1</a>")
+  print("<td>")
+  print(text)
+  print("</td>\n</tr>")
+end
+
+-- read the standard input
+local buffer = io.read("*all")
+
+-- initialize variables
+local pos = 0
+local entry, entries = nil, {}
+local news_entries = 5 -- number of printed changelog entries
+
+
+-- parse changelog
+for _=1,news_entries do
+  entry, pos = parse_entry(buffer, pos)
+  entries[#entries+1] = entry
+end
+
+-- it is not really clean to just print it, but I don't feel a necessity to use
+-- templates yet
+print("<table class='changelog'>")
+-- print parsed entries
+for _, entry in ipairs(entries) do
+  print_entry(entry)
+end
+print("</table>")
