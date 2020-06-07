40b39e3af81b87b03066ff4264596a2947c9be6d
michal_h21 <michal_h21@b3dcb3a8-5acb-4090-8ad3-ac17c6a25672>
2020-06-07 21:25:45 +0000

    Enable checking of multiple log files
    git-svn-id: https://svn.gnu.org.ua/sources/tex4ht/trunk@735 b3dcb3a8-5acb-4090-8ad3-ac17c6a25672

--- lit/checklog.lua
+++ lit/checklog.lua
@@ -9,34 +9,41 @@ kpse.set_program_name "luatex"
 -- the following library is part of make4ht
 local error_logparser = require("make4ht-errorlogparser")
 
-local input_file = arg[1]
+local function parse_log(content)
+  -- log parsing can be expensive on time, don't do it if we don't have
+  -- any error message in the log file
+  if content:match("\n!") then
+    local errors = error_logparser.parse(content)
+    if #errors > 0 then
+      print("Checking " .. input_file)
+      print("Errors found:")
+      for _, err in ipairs(errors) do
+        print(err.filename or "?", err.line or "?", err.error)
+      end
+      os.exit(1)
+    end
+  end
+end
+
 local content 
 -- the log file can be passed as filename argument, or piped from shell
-if input_file then
-  local ext = input_file:match("%.([^%.]+)$")
-  if not ext then
-    input_file = input_file .. ".log"
-  elseif ext ~="log" then
-    input_file = input_file:gsub("[^%.]+$", "log")
-  end
 
-  local f = io.open(input_file, "r")
-  content = f:read("*all")
+if #arg > 0 then
+  for _, input_file in ipairs(arg) do
+    local ext = input_file:match("%.([^%.]+)$")
+    if not ext then
+      input_file = input_file .. ".log"
+    elseif ext ~="log" then
+      input_file = input_file:gsub("[^%.]+$", "log")
+    end
+    local f = io.open(input_file, "r")
+    content = f:read("*all")
+    f:close()
+    parse_log(content)
+  end
 else
   -- read from STDIN
   content = io.read("*all")
-end
--- log parsing can be expensive on time, don't do it if we don't have
--- any error message in the log file
-if content:match("\n!") then
-  local errors = error_logparser.parse(content)
-  if #errors > 0 then
-    print("Checking " .. input_file)
-    print("Errors found:")
-    for _, err in ipairs(errors) do
-      print(err.filename or "?", err.line or "?", err.error)
-    end
-    os.exit(1)
-  end
+  parse_log(content)
 end
 
