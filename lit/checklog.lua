ff24018c5a52bc2bb3e4b7426346ba4c14442557
michal_h21 <michal_h21@b3dcb3a8-5acb-4090-8ad3-ac17c6a25672>
2020-06-07 10:49:48 +0000

    Added script for error detection in the log files
    git-svn-id: https://svn.gnu.org.ua/sources/tex4ht/trunk@729 b3dcb3a8-5acb-4090-8ad3-ac17c6a25672

---
+++ lit/checklog.lua
@@ -0,0 +1,30 @@
+-- this script parses log files for errors
+-- pass the .log file as a first argument
+-- 
+-- Usage:
+-- texlua checklog.lua <filename>.log
+--
+-- 
+kpse.set_program_name "luatex"
+-- the following library is part of make4ht
+local error_logparser = require("make4ht-errorlogparser")
+
+local input_file = arg[1]
+
+local f = io.open(input_file, "r")
+local content = f:read("*all")
+print("Checking log file: " .. input_file)
+-- log parsing can be expensive on time, don't do it if we don't have
+-- any error message in the log file
+if content:match("\n!") then
+  local errors = error_logparser.parse(content)
+  if #errors > 0 then
+    print("Errors found:")
+    for _, err in ipairs(errors) do
+      print(err.filename or "?", err.line or "?", err.error)
+    end
+    os.exit(1)
+  end
+end
+print("No errors found")
+
