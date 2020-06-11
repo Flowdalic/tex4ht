ca9d63d9ed03345ee3b8404ae4f340b02de80e5b
karl <karl@b3dcb3a8-5acb-4090-8ad3-ac17c6a25672>
2020-06-11 15:28:50 +0000

    Makefile and checklog tweaks
    git-svn-id: https://svn.gnu.org.ua/sources/tex4ht/trunk@738 b3dcb3a8-5acb-4090-8ad3-ac17c6a25672

--- lit/checklog.lua
+++ lit/checklog.lua
@@ -1,10 +1,11 @@
+-- $Id$
+-- Public domain. Originally written by Michal Hoftich, 2020.
 -- this script parses log files for errors
 -- pass the .log file as a first argument
 -- 
 -- Usage:
 -- texlua checklog.lua <filename>.log
---
--- 
+
 kpse.set_program_name "luatex"
 -- the following library is part of make4ht
 local error_logparser = require("make4ht-errorlogparser")
@@ -15,10 +16,10 @@ local function parse_log(input_file, content)
   if content:match("\n!") then
     local errors = error_logparser.parse(content)
     if #errors > 0 then
-      print("Checking " .. input_file)
-      print("Errors found:")
+      print(input_file .. ": errors found:")
       for _, err in ipairs(errors) do
-        print(err.filename or "?", err.line or "?", err.error)
+        print((err.filename or "?") .. ":"
+              .. (err.line or "?") .. ":" .. err.error)
       end
       os.exit(1)
     end
